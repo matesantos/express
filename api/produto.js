fba9109e31fd18cbe84f662a1901ee82f7cb9e41
Mateus <matesantos@hotmail.com>
2020-05-23 14:06:38 -0300

    exercícos de express

---
+++ api/produto.js
@@ -0,0 +1,14 @@
+module.exports = (app, texto)=> {
+    function salvar(req, res){
+        res.send('Produto > salvar > ' + texto)
+    }
+    
+    function obter(req, res){
+        res.send('Produto > obter > ' + texto)
+    }
+
+    app.get('/produto', obter)
+    app.post('/produto', salvar)
+
+    return { salvar, obter }
+}
